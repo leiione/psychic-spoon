421fa0cb271f97fdc94e0c0422f75c27ff5dcb9e
Kamran Farooq <leiione@leiiones-mac.local>
2023-03-24 07:58:13 +0500

    m

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get ('/hello', (req, res) => res.send('Hello World'))
+app.get ('/hello', (req, res) => res.send('Hello World 1266645454563'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports.handler = serverless(app)
 
