dfc6b241915e651f7cb62c8cad0ebdfaf5cb9c38
Kamran Farooq <leiione@leiiones-mac.local>
2023-03-24 08:28:57 +0500

    b

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get ('/hello', (req, res) => res.send('Hello VSAT TODAY MORNING DSM'))
+app.get ('/hello', (req, res) => res.send('Hello VSAT TODAY MORNING DSM Template Change'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports.handler = serverless(app)
