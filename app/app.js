f90e643c6b26bbfeb2dd2c619ab9562170cf6aa2
Kamran Farooq <leiione@leiiones-mac.local>
2023-03-24 08:01:01 +0500

    m

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get ('/hello', (req, res) => res.send('Hello World 1266645454563'))
+app.get ('/hello', (req, res) => res.send('Hello World DSM Deployment 24th March'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports.handler = serverless(app)
-
